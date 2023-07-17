c9ff747f12cd5ed4828f140c915c6b3b706957cd
Joseph Tan <joseph.txh@gmail.com>
2023-07-17 15:52:09 +0800

    removed unnecessary stylings

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
 
 function Header() {
-  const headerStyle = {
-    backgroundColor: '#f5ba13',
-    margin: 'auto -16px',
-    padding: '16px 32px',
-    boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.3)',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  };
-
-  const headingStyle = {
-    fontSize: '2rem',
-    color: '#fff',
-    fontFamily: 'McLaren, cursive',
-    fontWeight: 200,
-  };
 
   return (
-    <header style={headerStyle}>
-      <h1 style={headingStyle}>Keeper</h1>
+    <header>
+      <h1>Keeper</h1>
     </header>
   );
 }
